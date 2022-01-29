7f5977ebf6cd9f2937d96fd6ede64b8b971b3945
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-01-29 23:59:06 +0700

    Implement login page layout

--- app/auth/pages/login.tsx
+++ app/auth/pages/login.tsx
@@ -1,23 +1,40 @@
 import { useRouter, BlitzPage } from "blitz"
-import Layout from "app/core/layouts/Layout"
-import { LoginForm } from "app/auth/components/LoginForm"
+
+import Image from "next/image"
 
 const LoginPage: BlitzPage = () => {
   const router = useRouter()
+  const next = router.query.next ? decodeURIComponent(router.query.next as string) : "/"
+  const loginUrl = `/api/auth/google?redirectUrl=${encodeURIComponent(next)}`
 
   return (
-    <div>
-      <LoginForm
-        onSuccess={(_user) => {
-          const next = router.query.next ? decodeURIComponent(router.query.next as string) : "/"
-          router.push(next)
-        }}
-      />
+    <div className="max-w-[428px] m-auto p-6 pt-16">
+      <Image src="/images/logo_vertical.svg" width={106} height={96} alt="MayDay" />
+      <div className="py-3 gap-2">
+        <h3 className="title3">Welcome to MayDay</h3>
+        <p className="text-regular leading-normal font-regular text-ink-darkest">
+          Sign in to check out your items.
+        </p>
+      </div>
+      <div className="py-3">
+        <a
+          className="
+        h-12 px-[52px] rounded-lg border border-sky-light
+        flex items-center justify-center relative
+        font-sans text-regular leading-none font-medium text-ink-darkest
+        "
+          href={loginUrl}
+        >
+          <span className="h-5 absolute left-4">
+            <Image src="/images/google.svg" width={20} height={20} alt="" />
+          </span>
+          Continue with Google
+        </a>
+      </div>
     </div>
   )
 }
 
 LoginPage.redirectAuthenticatedTo = "/"
-LoginPage.getLayout = (page) => <Layout title="Log In">{page}</Layout>
 
 export default LoginPage

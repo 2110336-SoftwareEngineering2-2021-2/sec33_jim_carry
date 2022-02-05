13c1830fb1e762dd1ef455db4131722a27bd2177
Nut Pinyo <bomb.np@gmail.com>
2022-02-05 21:59:31 +0700

    add poc for api routes

---
+++ app/api/me.ts
@@ -0,0 +1,10 @@
+import { BlitzApiHandler, invokeWithMiddleware } from "blitz"
+import getCurrentUser from "app/users/queries/getCurrentUser"
+
+const handler: BlitzApiHandler = async (req, res) => {
+  const user = await invokeWithMiddleware(getCurrentUser, {}, { req, res })
+  res.statusCode = 200
+  res.setHeader("Content-Type", "application/json")
+  res.end(JSON.stringify(user))
+}
+export default handler
---
+++ app/api/ping.ts
@@ -0,0 +1,7 @@
+import { BlitzApiHandler } from "blitz"
+
+const handler: BlitzApiHandler = async (req, res) => {
+  res.statusCode = 200
+  res.end("Welcome to Mayday API!")
+}
+export default handler

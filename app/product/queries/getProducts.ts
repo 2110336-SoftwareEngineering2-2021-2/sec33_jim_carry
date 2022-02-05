909c606b9a2ab337266610da455605ea8448c232
cyred <cyredhat@gmail.com>
2022-02-05 22:40:20 +0700

    Add product queries

---
+++ app/product/queries/getProducts.ts
@@ -0,0 +1,31 @@
+import { paginate, resolver } from "blitz"
+import db, { Prisma } from "db"
+
+interface GetProductsInput
+  extends Pick<Prisma.ProductFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
+
+export default resolver.pipe(
+  resolver.authorize(),
+  async ({ where, orderBy, skip = 0, take = 100 }: GetProductsInput) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    where = { ...where, hidden: false }
+    const {
+      items: products,
+      hasMore,
+      nextPage,
+      count,
+    } = await paginate({
+      skip,
+      take,
+      count: () => db.product.count({ where }),
+      query: (paginateArgs) => db.product.findMany({ ...paginateArgs, where, orderBy }),
+    })
+
+    return {
+      products,
+      nextPage,
+      hasMore,
+      count,
+    }
+  }
+)

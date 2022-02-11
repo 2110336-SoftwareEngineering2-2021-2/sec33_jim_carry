9149286245fd58f6cea3b6fe15155cbbc6da29e0
Nut Pinyo <bomb.np@gmail.com>
2022-02-11 22:36:14 +0700

    feat: add get/add/remove wishlist

---
+++ app/wishlist/queries/getWishlist.ts
@@ -0,0 +1,21 @@
+import { resolver, NotFoundError, Ctx, AuthorizationError } from 'blitz'
+import db from 'db'
+
+export default resolver.pipe(
+  resolver.authorize(),
+  async (_ = null, { session }: Ctx) => {
+    if (!session.userId) throw new AuthorizationError()
+    const user = await db.user.findFirst({
+      where: {
+        id: session.userId,
+      },
+      include: {
+        wishlist: true,
+      },
+    })
+
+    if (!user) throw new NotFoundError()
+
+    return user.wishlist
+  }
+)

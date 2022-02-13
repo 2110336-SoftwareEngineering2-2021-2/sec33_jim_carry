77b629bf705a4a3b01b80c6495be5e223dd7b372
komson <shootingstar7447@gmail.com>
2022-02-13 13:52:52 +0700

    Add top bar for Product Detail

--- app/product/pages/products/[pid].tsx
+++ app/product/pages/products/[pid].tsx
@@ -1,6 +1,9 @@
 import { BlitzPage, useParam } from 'blitz'
 import { Suspense } from 'react'
+import { FiShoppingBag } from 'react-icons/fi'
 
+import { TopBar } from 'app/core/components/TopBar'
+import { TopBarAction } from 'app/core/components/TopBarAction'
 import { setupLayout } from 'app/core/utils/setupLayout'
 import { ProductView } from 'app/product/components/ProductView'
 
@@ -13,6 +16,13 @@ const ProductDetail: BlitzPage = () => {
 
   return (
     <div>
+      <TopBar
+        actions={
+          <TopBarAction>
+            <FiShoppingBag />
+          </TopBarAction>
+        }
+      />
       <Suspense fallback={null}>
         <ProductView pid={pid} />
       </Suspense>

407e58c040dd28a17d2ec47202f96487217e0c51
IristuN <siri.iristun@gmail.com>
2022-02-14 00:02:12 +0700

    add searchProducts

---
+++ app/product/queries/searchProducts.ts
@@ -0,0 +1,36 @@
+import { resolver } from 'blitz'
+import db from 'db'
+import { z } from 'zod'
+
+const SearchProducts = z.object({
+  name: z.string(),
+  take: z.number(),
+})
+
+export default resolver.pipe(
+  resolver.zod(SearchProducts),
+  resolver.authorize(),
+  async (input) => {
+    // Do your stuff :)
+    const queries = {
+      take: input.take,
+      where: {
+        name: {
+          contains: input.name,
+        },
+        hidden: false,
+      },
+      select: {
+        id: true,
+        name: true,
+        updatedAt: true,
+        shop: true,
+        image: true,
+        stock: true,
+        price: true,
+      },
+    }
+
+    return await db.product.findMany(queries)
+  }
+)

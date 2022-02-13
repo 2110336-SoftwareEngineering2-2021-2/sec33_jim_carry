527b4633b1c2a9604296df599d7b5fe7b6f13600
Nut Pinyo <bomb.np@gmail.com>
2022-02-13 15:41:03 +0700

    fix: use float instead of decimal for price

---
+++ db/migrations/20220213084009_use_float_instead_of_decimal/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `DoublePrecision`.
+  - You are about to alter the column `soldPrice` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `DoublePrecision`.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" ALTER COLUMN "price" SET DATA TYPE DOUBLE PRECISION,
+ALTER COLUMN "soldPrice" SET DATA TYPE DOUBLE PRECISION;

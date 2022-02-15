4d830132a7c4883ac361d6310830035548ea1588
IristuN <siri.iristun@gmail.com>
2022-02-15 21:54:47 +0700

    add orderBy to searchProducts

--- app/product/queries/searchProducts.ts
+++ app/product/queries/searchProducts.ts
@@ -1,11 +1,13 @@
 import { resolver } from 'blitz'
-import db from 'db'
+import db, { Shop } from 'db'
 import { z } from 'zod'
 
 const SearchProducts = z.object({
   name: z.string(),
   take: z.number(),
   skip: z.number(),
+  orderBy: z.enum(['name', 'createdAt', 'price', 'rating']),
+  orderType: z.enum(['asc', 'desc']),
 })
 
 export default resolver.pipe(
@@ -13,6 +15,27 @@ export default resolver.pipe(
   resolver.authorize(),
   async (input) => {
     // Do your stuff :)
+    const order = []
+
+    switch (input.orderBy) {
+      case 'name': {
+        order.push({ name: input.orderType })
+      }
+      case 'createdAt': {
+        order.push({ createdAt: input.orderType })
+      }
+      case 'price': {
+        order.push({ price: input.orderType })
+      }
+      case 'rating': {
+        order.push({
+          shop: {
+            rating: input.orderType,
+          },
+        })
+      }
+    }
+
     const queries = {
       take: input.take,
       skip: input.skip,
@@ -22,15 +45,10 @@ export default resolver.pipe(
         },
         hidden: false,
       },
-      select: {
-        id: true,
-        name: true,
-        updatedAt: true,
+      include: {
         shop: true,
-        images: true,
-        stock: true,
-        price: true,
       },
+      orderBy: order,
     }
 
     return await db.product.findMany(queries)

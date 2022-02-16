03b80e71cf835a8fb3bc528465e218a57cbb106f
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-02-16 22:58:12 +0700

    Fix finish page button and remove unused imports

--- app/users/pages/register/finish.tsx
+++ app/users/pages/register/finish.tsx
@@ -1,10 +1,7 @@
-import { BlitzPage, Image, Link, Routes } from 'blitz'
+import { BlitzPage, Link, Routes } from 'blitz'
 
 import { Button } from 'app/core/components/Button'
-import Form, { FORM_ERROR } from 'app/core/components/Form'
-import LabeledTextField from 'app/core/components/LabeledTextField'
 import { TopBar } from 'app/core/components/TopBar'
-import { useGoBack } from 'app/core/hooks/useGoBack'
 import { setupAuthRedirect } from 'app/core/utils/setupAuthRedirect'
 import { setupLayout } from 'app/core/utils/setupLayout'
 
@@ -18,7 +15,7 @@ const FinishRegisterPage: BlitzPage = () => {
       </div>
       <div className="px-6 my-8">
         <Link href={Routes.Menu().pathname} passHref>
-          <Button as="a" type="submit" fullWidth>
+          <Button as="a" fullWidth>
             Back to Browsing
           </Button>
         </Link>

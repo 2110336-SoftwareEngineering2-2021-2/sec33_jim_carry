876885f702202c93c7282bc9e1840dbbc2e2f5cc
komson <shootingstar7447@gmail.com>
2022-02-27 18:11:29 +0700

    Add order status page

---
+++ app/order/components/OrderView.tsx
@@ -0,0 +1,52 @@
+import { useQuery } from 'blitz'
+import { useMemo } from 'react'
+import { FiTruck } from 'react-icons/fi'
+
+import { EmptyState } from 'app/core/components/EmptyState'
+
+import getOrders from '../queries/getOrders'
+import { OrderCard } from './OrderCard'
+
+export interface OrderViewProps {
+  filter: number
+}
+
+export function OrderView({ filter }: OrderViewProps) {
+  const status = useMemo(() => {
+    switch (filter) {
+      case 1:
+        return 'PENDING'
+      case 2:
+        return 'SHIPPED'
+      case 3:
+        return 'COMPLETED'
+      case 4:
+        return 'CANCELLED'
+      default:
+        return 'PENDING'
+    }
+  }, [filter])
+  const [orders] = useQuery(getOrders, {
+    status: status,
+  })
+  console.log(orders)
+  if (orders.length == 0) {
+    return (
+      <EmptyState
+        icon={<FiTruck strokeWidth={0.5} size={84} />}
+        title={`There are no items in your order.`}
+        description={
+          <>
+            Start browsing now to discover our <br />
+            high-quality second hand goods!
+          </>
+        }
+      />
+    )
+  }
+  return (
+    <div className="flex flex-col divide-y divide-sky-light">
+      {orders ? orders.map((e, idx) => <OrderCard key={idx} order={e} />) : ''}
+    </div>
+  )
+}
--- app/order/pages/orders.tsx
+++ app/order/pages/orders.tsx
@@ -1,13 +1,38 @@
 import { BlitzPage } from 'blitz'
+import { Suspense, useState } from 'react'
 
+import {
+  SegmentedControl,
+  SegmentedControlItem,
+} from 'app/core/components/SegmentedControl'
 import { TopBar } from 'app/core/components/TopBar'
 import { setupAuthRedirect } from 'app/core/utils/setupAuthRedirect'
 import { setupLayout } from 'app/core/utils/setupLayout'
 
+import { OrderView } from '../components/OrderView'
+
 const OrdersPage: BlitzPage = () => {
+  const [value, setvalue] = useState(1)
   return (
     <div>
-      <TopBar title="Orders" largeTitle />
+      <TopBar title="My Orders" largeTitle />
+      <div className="pt-4 pb-3 px-6">
+        <SegmentedControl
+          value={value}
+          onChange={(newvalue) => {
+            setvalue(newvalue)
+            console.log(value)
+          }}
+        >
+          <SegmentedControlItem value={1}>Pending</SegmentedControlItem>
+          <SegmentedControlItem value={2}>Shipping</SegmentedControlItem>
+          <SegmentedControlItem value={3}>Completed</SegmentedControlItem>
+          <SegmentedControlItem value={4}>Canceled</SegmentedControlItem>
+        </SegmentedControl>
+      </div>
+      <Suspense fallback="">
+        <OrderView filter={value} />
+      </Suspense>
     </div>
   )
 }

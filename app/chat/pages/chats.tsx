0821ca0b92609608ce9a1e910dc89109872fc310
Nut Pinyo <bomb.NP@gmail.com>
2022-02-28 22:17:34 +0700

    add chat list features (#24)
    * feat: add chat schemas
    
    * feat: add last message read id
    
    * feat: add chat list query and mutation
    
    * feat: add create chat func to message button
    
    * feat: chat list page
    
    * feat: add useChatStore
    
    * fix: remove console log
    
    * fix: merge migration
    
    * fix: add todo for createChat to redirect to chat page
    
    * fix: use isLoading state instead of suspense

--- app/chat/pages/chats.tsx
+++ app/chat/pages/chats.tsx
@@ -1,11 +1,28 @@
-import { BlitzPage } from 'blitz'
+import { BlitzPage, useQuery } from 'blitz'
 
+import { ChatList } from 'app/chat/components/ChatList'
+import { Spinner } from 'app/core/components/Spinner'
 import { MainPageLayout } from 'app/core/layouts/MainPageLayout'
+import { setupAuthRedirect } from 'app/core/utils/setupAuthRedirect'
+
+import { useChatStore } from '../context/useChatStore'
 
 const Chats: BlitzPage = () => {
-  return <div>Chats</div>
+  const [chats, isLoading] = useChatStore((state) => [
+    state.chats,
+    state.isLoading,
+  ])
+  if (isLoading) {
+    return <Spinner />
+  }
+  return (
+    <main>
+      <ChatList chats={chats} />
+    </main>
+  )
 }
 
+setupAuthRedirect(Chats)
 Chats.getLayout = (page) => (
   <MainPageLayout title="Chats">{page}</MainPageLayout>
 )

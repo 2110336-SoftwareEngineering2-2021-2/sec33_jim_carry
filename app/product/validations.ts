ce8e1c378271b185d4003600b1ce52d25e872a03
Nattamon Polv <joppy.pgh@gmail.com>
2022-03-01 19:42:36 +0700

    Remove comments
    Co-authored-by: Suphon T. <paphonb@gmail.com>

--- app/product/validations.ts
+++ app/product/validations.ts
@@ -9,13 +9,3 @@ export const ProductFormValues = z.object({
   description: z.string().optional(),
   //images: z.string().array(),
 })
-
-// export const UpdateProduct = z.object({
-//   id: z.number(),
-//   name: z.string().nonempty(),
-//   price: z.string().nonempty(),
-//   stock: z.string().nonempty(),
-//   hashtags: z.string().optional(),
-//   description: z.string().optional(),
-//   //images: z.string().array(),
-// })

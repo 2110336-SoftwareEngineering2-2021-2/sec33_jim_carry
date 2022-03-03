a6c86ead164a406400a5ad3dab95496f78389cea
Anon-136 <anonjin44@gmail.com>
2022-03-03 09:07:32 +0700

    Add withdraw amount error

--- app/withdrawal/formValidations.ts
+++ app/withdrawal/formValidations.ts
@@ -2,5 +2,14 @@ import { z } from 'zod'
 
 export const FormWithdrawal = z.object({
   bankAccount: z.string().nonempty(),
-  amount: z.string().nonempty(),
+  amount: z
+    .string()
+    .nonempty()
+    .transform((value) => Number(value))
+    .refine((value) => !Number.isNaN(value), {
+      message: 'Amount must be a number',
+    })
+    .refine((value) => value > 0, {
+      message: 'Amount must be positive number',
+    }),
 })

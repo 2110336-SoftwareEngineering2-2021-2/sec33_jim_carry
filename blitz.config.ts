260c2486c02426d2b48b50f02ade1c1f50a14964
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-03-25 22:50:21 +0700

    Upload image to cloud storage

--- blitz.config.ts
+++ blitz.config.ts
@@ -25,7 +25,7 @@ const config: BlitzConfig = {
   },
   */
   images: {
-    domains: ['images.unsplash.com', 'picsum.photos'],
+    domains: ['images.unsplash.com', 'picsum.photos', 'storage.googleapis.com'],
   },
 }
 module.exports = config

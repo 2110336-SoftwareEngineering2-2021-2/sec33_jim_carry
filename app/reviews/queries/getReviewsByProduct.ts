6d56528e640a57e6b732eb61fc1218ea8fa5ba10
cyred <cyredhat@gmail.com>
2022-03-26 19:05:40 +0700

    Add getReviewsByProduct query

---
+++ app/reviews/queries/getReviewsByProduct.ts
@@ -0,0 +1,22 @@
+import { paginate, resolver } from 'blitz'
+import db, { Prisma } from 'db'
+import { z } from 'zod'
+
+const GetReviewsByProductInput = z.object({
+  productId: z.number(),
+})
+
+const getReviewsByProduct = resolver.pipe(
+  resolver.zod(GetReviewsByProductInput),
+  resolver.authorize(),
+  async ({ productId }) => {
+    const reviews = await db.review.findMany({
+      where: { productId },
+      orderBy: { createdAt: 'desc' },
+    })
+
+    return reviews
+  }
+)
+
+export default getReviewsByProduct

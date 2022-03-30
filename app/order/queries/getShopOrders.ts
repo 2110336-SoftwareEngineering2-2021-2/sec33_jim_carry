1860cb82f4d77c69c3a3fe4e95757363ec9662b0
Siri Thammarerkrit <56879722+IristuN@users.noreply.github.com>
2022-03-30 11:31:29 +0700

    remove getShopOrders Authorization Error
    Co-authored-by: Suphon T. <paphonb@gmail.com>

--- app/order/queries/getShopOrders.ts
+++ app/order/queries/getShopOrders.ts
@@ -4,8 +4,6 @@ import db, { Prisma } from 'db'
 const getShopOrders = resolver.pipe(
   resolver.authorize(),
   async (_ = null, { session: { userId } }) => {
-    // Authorize user
-    if (!userId) throw new AuthorizationError()
     //get Shop from userId
     const shop = await db.shop.findUnique({
       where: { userId },

ea7a111872dbd0ef76409a8e569e43bef2cc0863
Nut Pinyo <bomb.np@gmail.com>
2022-03-30 20:36:03 +0700

    fix: shop rating display

--- app/shop/components/shop/ShopBio.tsx
+++ app/shop/components/shop/ShopBio.tsx
@@ -8,19 +8,31 @@ interface ShopBioProps {
 }
 
 export const ShopBio = ({ bio, reviews }: ShopBioProps) => {
-  const avgRating =
-    reviews.reduce((acc, cur) => acc + cur.rating, 0) / reviews.length
-  const noRating = reviews.length === 0
   return (
     <div>
       <div className="text-regular text-ink-darkest">{bio}</div>
-      <div className="mt-1 flex space-x-2 items-center">
-        <Star rating={avgRating} noRating={noRating} />
-        <span className="text-small text-ink-light">
-          {noRating ? '-' : avgRating.toFixed(1)}{' '}
-          {`(${reviews.length} review${reviews.length > 1 ? 's' : ''})`}
-        </span>
-      </div>
+      <ShopRating reviews={reviews} />
+    </div>
+  )
+}
+
+const ShopRating = ({ reviews }: { reviews: Review[] }) => {
+  const avgRating =
+    reviews.reduce((acc, cur) => acc + cur.rating, 0) / reviews.length
+  const noRating = reviews.length === 0
+
+  if (noRating) {
+    return <div className="text-small text-ink-light">No review</div>
+  }
+
+  return (
+    <div className="mt-1 flex space-x-2 items-center">
+      <Star rating={avgRating} />
+      <span className="text-small text-ink-light">
+        {`${avgRating.toFixed(1)} (${reviews.length} review${
+          reviews.length > 1 ? 's' : ''
+        })`}
+      </span>
     </div>
   )
 }

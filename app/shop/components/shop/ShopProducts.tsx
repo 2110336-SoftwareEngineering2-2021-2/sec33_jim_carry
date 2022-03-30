cdef8c076645e59098b8b9ec2fa5caf8142647b6
Nut Pinyo <bomb.np@gmail.com>
2022-03-30 23:41:33 +0700

    feat: add product empty state in shop

--- app/shop/components/shop/ShopProducts.tsx
+++ app/shop/components/shop/ShopProducts.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from 'blitz'
 import { Product } from 'db'
+import { FiGrid } from 'react-icons/fi'
 
+import { EmptyState } from 'app/core/components/EmptyState'
 import getProducts from 'app/product/queries/getProducts'
 
 import { ShopProductCard } from './ShopProductCard'
@@ -10,11 +12,15 @@ interface ShopProductsProps {
 }
 
 export const ShopProducts = ({ shopId }: ShopProductsProps) => {
-  const [{ products }] = useQuery(
-    getProducts,
-    { where: { shopId } },
-    { suspense: true }
-  )
+  const [{ products }] = useQuery(getProducts, { where: { shopId } })
+  if (products.length === 0)
+    return (
+      <EmptyState
+        icon={<FiGrid strokeWidth={0.5} size={84} />}
+        title={`There are products in this shop (yet!)`}
+        description={<>Check back later for new products!</>}
+      />
+    )
   return (
     <div className="p-4 grid gap-3 grid-cols-2">
       {products.map((product) => (

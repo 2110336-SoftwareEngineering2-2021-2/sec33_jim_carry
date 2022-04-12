3db670b191c48c391eefdcf8fa57c282a9cb3c6c
Nut Pinyo <bomb.np@gmail.com>
2022-04-12 13:16:43 +0700

    fix: doc

--- app/order/queries/countSoldOrders.ts
+++ app/order/queries/countSoldOrders.ts
@@ -1,13 +1,13 @@
 import { AuthorizationError, Ctx, resolver } from 'blitz'
 import db, { Prisma } from 'db'
 
+interface CountProductsInput extends Pick<Prisma.OrderFindManyArgs, 'where'> {}
+
 /**
  * Count orders that satisfies the filter
  *
  * @returns The orders with the specify filter along with shop and orderitemsnapshot
  */
-interface CountProductsInput extends Pick<Prisma.OrderFindManyArgs, 'where'> {}
-
 const countOrders = resolver.pipe(
   async ({ where }: CountProductsInput, { session }: Ctx) => {
     const count = await db.order.count({

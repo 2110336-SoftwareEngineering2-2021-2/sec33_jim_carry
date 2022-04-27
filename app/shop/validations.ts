c581931e68f70e324cd2d17f09665b907beb39d5
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-04-27 14:28:09 +0700

    Add citizen id validation

--- app/shop/validations.ts
+++ app/shop/validations.ts
@@ -1,9 +1,28 @@
 import { z } from 'zod'
 
+const invalidCitizenId = 'Invalid Citizen ID'
+
 export const CreateShop = z.object({
   bio: z.string().nonempty(),
   phoneNo: z.string().nonempty(),
   name: z.string().nonempty(),
-  citizenId: z.string().nonempty(),
+  citizenId: z
+    .string()
+    .length(13, invalidCitizenId)
+    .regex(/^[0-9]{13}$/, invalidCitizenId)
+    .refine(checkCitizenId, invalidCitizenId),
   image: z.string().optional(),
 })
+
+function checkCitizenId(id: string) {
+  let sum = 0
+  for (let i = 0, sum = 0; i < 12; i++) {
+    sum += parseInt(id.charAt(i)) * (13 - i)
+  }
+  const mod = sum % 11
+  const check = (11 - mod) % 10
+  if (check == parseInt(id.charAt(12))) {
+    return true
+  }
+  return false
+}
